46e8af773e981ef91648ba6886371e043aed381b
stutisaurya <stuti.saurya@gmail.com>
2021-07-23 22:30:24 +0530

    Added:InsertTable

--- AddressBook.sql
+++ AddressBook.sql
@@ -6,7 +6,7 @@ SELECT DATABASE();
 
 #Uc2
 
-	CREATE TABLE address_book
+	CREATE TABLE Add_Book
 	(
     	id               INT unsigned NOT NULL AUTO_INCREMENT,	
     	first_name       VARCHAR(50) NOT NULL,			
@@ -15,9 +15,18 @@ SELECT DATABASE();
     	city             VARCHAR(30) NOT NULL,			
     	state            VARCHAR(30) NOT NULL,			
     	zip              INT(6) NOT NULL,				
-    	phone_number     INT(13) NOT NULL,		
+    	phone_number     VARCHAR(13) NOT NULL,		
     	email            VARCHAR(50) NOT NULL,			
     	PRIMARY KEY(id)					
 	);
     
-	DESCRIBE address_book;
+	DESCRIBE Add_Book;
+#Uc3
+
+	INSERT INTO Add_Book(first_name, last_name, address, city , state , zip , phone_number, email) VALUES
+	    ('Stuti','Saurya','Rajgir', 'Delhi', 'abc',560058, 9695592954, 'stuti@gmail.com'),
+		('Sakshi','Priyal','Gaya','Goa','ghi', 560057,9745277712,'sakshi123@gmail.com'),
+    	('Sahil','Anand','Rajgir','Bangalore','pqr',560059,9864727523,'sahil12@gmail.com');
+	    SELECT * FROM Add_Book;
+        DROP TABLE address_book;
+
